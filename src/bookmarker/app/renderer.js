a70e46f44a49e7edf5714d39d2be1c7b91e24979
pearpages <pere@soms.cat>
2017-05-12 12:36:16 +0200

    renderer refacto

--- src/bookmarker/app/renderer.js
+++ src/bookmarker/app/renderer.js
@@ -1,50 +1,64 @@
-const newLinkUrl = document.querySelector('#new-link-url'); // Returns the first Element within the document that matches the specified selector, or group of selectors.
-newLinkUrl.value = 'https://pearpages.com';
-const newLinkSubmit = document.querySelector('.new-link-form--submit');
-newLinkSubmit.disabled = false;
-const form = document.querySelector('.new-link-form');
-const linkTemplate = document.querySelector('#link-template');
-const linksSection = document.querySelector('.links');
-
-newLinkUrl.addEventListener('keyup', function whetherActivate() {
-    newLinkSubmit.disabled = !newLinkUrl.validity.valid;
-});
-
 const parser = new DOMParser();
-function parseResponse(text) {
-    return parser.parseFromString(text,'text/html');
+
+const newLinkUrl = handleNewLinksUrl();
+handleForm(newLinkUrl, parser);
+
+function handleForm(newLinkUrl, parser) {
+    const form = document.querySelector('.new-link-form');
+    form.addEventListener('submit', handleSubmit);
+
+    function addToPage({ title, url }) {
+        const newLink = getNewLinkTemplate();
+        const titleElement = newLink.querySelector('.link--title');
+        const urlElement = newLink.querySelector('.link--url');
+
+        titleElement.textContent = title;
+        urlElement.href = url;
+        urlElement.textContent = url;
+
+        const linksSection = document.querySelector('.links');
+        linksSection.appendChild(newLink);
+        return { title, url };
+    }
+
+    function getNewLinkTemplate() {
+        const linkTemplate = document.querySelector('#link-template');
+        const newLink = linkTemplate.content.cloneNode(true); // deep clone
+        return newLink;
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        const url = newLinkUrl.value;
+        fetch(url)
+            .then(function getResponse(response) {
+                return response.text();
+            })
+            .then(function parseResponse(text) {
+                return parser.parseFromString(text, 'text/html');
+            })
+            .then(function findTitle(nodes) {
+                return nodes.querySelector('title').textContent;
+            })
+            .then(title => ({ title, url }))
+            .then(addToPage)
+            .then(title => console.log(title))
+            .catch(function showError(error) {
+                console.error(error);
+            })
+    }
 }
-function findTitle(nodes) {
-    return nodes.querySelector('title').textContent;
+
+function handleNewLinksUrl() {
+    const newLinkUrl = document.querySelector('#new-link-url'); // Returns the first Element within the document that matches the specified selector, or group of selectors.
+    newLinkUrl.value = 'https://pearpages.com';
+
+    const newLinkSubmit = document.querySelector('.new-link-form--submit');
+    newLinkSubmit.disabled = false;
+
+    newLinkUrl.addEventListener('keyup', function whetherActivate() {
+        newLinkSubmit.disabled = !newLinkUrl.validity.valid;
+    });
+    return newLinkUrl;
 }
-
-function addToPage({title,url}) {
-    const newLink = linkTemplate.content.cloneNode(true); // deep clone
-    const titleElement = newLink.querySelector('.link--title');
-    const urlElement = newLink.querySelector('.link--url');
-
-    titleElement.textContent = title;
-    urlElement.href = url;
-    urlElement.textContent = url;
-
-    linksSection.appendChild(newLink);
-    return {title, url};
-}
-
-form.addEventListener('submit', function submit(event) {
-    event.preventDefault();
-
-    const url = newLinkUrl.value;
-    fetch(url)
-        .then(function getResponse(response) {
-            return response.text();
-        })
-        .then(parseResponse)
-        .then(findTitle)
-        .then(title => ({title, url}))
-        .then(addToPage)
-        .then(title => console.log(title))
-        .catch(function showError(error) {
-            console.error(error);
-        })
-});
